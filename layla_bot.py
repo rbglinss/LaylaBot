b4b48b0daf9e6167a812cd468afd4e5a68ee10b3
Ronaldo Batista <rbglinss@gmail.com>
2025-08-03 17:45:55 -0300

    Projeto completo com render.yaml

--- layla_bot.py
+++ layla_bot.py
@@ -1,12 +1,10 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "LaylaBot está rodando com sucesso no Render!"
+    return "LaylaBot is running!"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
